df47386e4f3654ecd1f8812fc8f84d2274649416
ImGili <18017158729@163.com>
2023-02-02 09:35:08 +0800

    ✨ feat: can draw

--- test/tests/eat_edge.cpp
+++ test/tests/eat_edge.cpp
@@ -4,6 +4,7 @@
 #include <CGAL/Polygon_mesh_processing/polygon_mesh_to_polygon_soup.h>
 #include <CGAL/Polygon_mesh_processing/polygon_soup_to_polygon_mesh.h>
 #include <unordered_set>
+#include <CGAL/draw_surface_mesh.h>
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef Kernel::Point_3 Point_3;
 typedef CGAL::Surface_mesh<Point_3> Surface_Mesh;
@@ -15,18 +16,20 @@ int eat_edge()
 
 	CGAL::Polygon_mesh_processing::IO::read_polygon_mesh("./mesh0.ply", mesh0);
 	CGAL::Polygon_mesh_processing::IO::read_polygon_mesh("./mesh1.ply", mesh1);
-	
+	for (auto aaa : mesh0.edges())
+	{
+		std::cout << aaa << std::endl;
+	} 
 	std::vector<Point_3> vertices;
 	std::vector<Facet> facets;
 	CGAL::Polygon_mesh_processing::polygon_mesh_to_polygon_soup(mesh0, vertices, facets);
-
 	Surface_Mesh meshtmp;
 	CGAL::Polygon_mesh_processing::polygon_soup_to_polygon_mesh(vertices, facets, meshtmp);
 
-	std::vector<Point_3> verticestmp;
-	std::vector<Facet> facetstmp;
-
-	CGAL::Polygon_mesh_processing::polygon_mesh_to_polygon_soup(meshtmp, verticestmp, facetstmp);
+	
+	CGAL::draw(mesh0);
+	CGAL::draw(mesh1);
+	CGAL::draw(meshtmp);
 	
 	return 0;
 }
