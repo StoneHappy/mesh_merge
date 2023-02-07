a512874848d66723548bb5f4ab5bc6a4cc59ed83
ImGili <18017158729@163.com>
2023-02-07 16:54:57 +0800

    ✨ feat: eat all edge

--- test/tests/eat_edge_mm.cpp
+++ test/tests/eat_edge_mm.cpp
@@ -7,14 +7,21 @@ int eat_edge_mm()
 	CGAL::Polygon_mesh_processing::IO::read_polygon_mesh("./polygon_mesh0.ply", mesh0);
 	CGAL::Polygon_mesh_processing::IO::read_polygon_mesh("./polygon_mesh1.ply", mesh1);
 
-	Surface_mesh outmesh;
-	mm::eatEdge(mesh0, mesh1, outmesh);
-	CGAL::draw(outmesh);
+	Surface_mesh outmesh0, outmesh1;
+	mm::eatEdge(mesh0, mesh1, outmesh0);
+	mm::eatEdge(mesh1, mesh0, outmesh1);
+	//CGAL::draw(outmesh0);
 
-	std::ofstream f("./out.ply", std::ios_base::binary);
-	CGAL::IO::set_binary_mode(f);
-	CGAL::IO::write_PLY(f, outmesh);
-	f.close();
+	std::ofstream f0("./out0.ply", std::ios_base::binary);
+	CGAL::IO::set_binary_mode(f0);
+	CGAL::IO::write_PLY(f0, outmesh0);
+	f0.close();
+
+
+	std::ofstream f1("./out1.ply", std::ios_base::binary);
+	CGAL::IO::set_binary_mode(f1);
+	CGAL::IO::write_PLY(f1, outmesh1);
+	f1.close();
 	return 0;
 }
 
